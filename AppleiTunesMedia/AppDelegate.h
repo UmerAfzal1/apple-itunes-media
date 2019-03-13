2ce72616d149b0d8768d868639e9a42286fe77dc
Umer.Afzal <umer.afzal@orangebuddies.pk>
2019-03-13 10:31:59 +0500

    first commit apple itunes media

---
+++ AppleiTunesMedia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppleiTunesMedia
+//
+//  Created by Umer Afzal on 13/03/2019.
+//  Copyright © 2019 Umer Afzal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppleiTunesMedia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppleiTunesMedia
+//
+//  Created by Umer Afzal on 13/03/2019.
+//  Copyright © 2019 Umer Afzal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
